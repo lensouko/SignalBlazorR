b23ebb4cd56d13c8a350700b86c497a1549927c2
煉倉庫 <len@nya-nya.netlen@nya-nya.net>
2018-03-27 02:35:16 +0900

    初回登録？

---
+++ SignalBlazorR/SignalBlazorR.Server/Hub/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using SignalBlazorR.Shared;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalBlazorR.Server.Hub
+{
+	public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+	{
+		public Task PostMessage(SimpleMessage msg)
+		{
+			// プレビュー１の場合
+			return Clients.All.SendAsync("AddMessage", msg);
+			// α2の場合
+			//return Clients.All.InvokeAsync("AddMessage", msg);
+		}
+	}
+}
---
+++ SignalBlazorR/SignalBlazorR.Shared/SimpleMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalBlazorR.Shared
+{
+	public class SimpleMessage
+	{
+		public string name { get; set; } = string.Empty;
+		public string message { get; set; } = string.Empty;
+	}
+}
