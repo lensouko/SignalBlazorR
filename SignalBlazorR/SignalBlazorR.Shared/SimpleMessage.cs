c515e8d2a1b80107cc710586e291fb1a8be66ec8
lensouko <len@nya-nya.net>
2018-04-02 02:56:55 +0900

    チャットを単独ページから埋め込みページへ移動、埋め込まれているページごとにグループを分けて、発言対象にグループと全体を切り分けられるように機能追加

--- SignalBlazorR/SignalBlazorR.Shared/SimpleMessage.cs
+++ SignalBlazorR/SignalBlazorR.Shared/SimpleMessage.cs
@@ -6,7 +6,21 @@ namespace SignalBlazorR.Shared
 {
 	public class SimpleMessage
 	{
+		public string room { get; set; }
 		public string name { get; set; } = string.Empty;
 		public string message { get; set; } = string.Empty;
+
+		public static readonly string AllGroup = "All";
+
+		public SimpleMessage Clone()
+		{
+			return new SimpleMessage
+			{
+				room = this.room,
+				name = this.name,
+				message = this.message
+			};
+		}
+
 	}
 }
