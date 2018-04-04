704c3d19b0b8862ed8ea577af6d921db269b4aa8
lensouko <len@nya-nya.net>
2018-04-04 23:06:53 +0900

    一度マスターで色々変更したものの、azureで動かなくなって復旧できなくなったのでsample001からブランチを分岐して組みなおし まずはChatのJSを別ファイル化してルーム処理を追加

--- SignalBlazorR/SignalBlazorR.Server/Hub/ChatHub.cs
+++ SignalBlazorR/SignalBlazorR.Server/Hub/ChatHub.cs
@@ -6,12 +6,30 @@ namespace SignalBlazorR.Server.Hub
 {
 	public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
 	{
-		public Task PostMessage(SimpleMessage msg)
+		public async Task PostMessage(SimpleMessage msg)
 		{
-			// プレビュー１の場合
-			return Clients.All.SendAsync("AddMessage", msg);
-			// α2の場合
-			//return Clients.All.InvokeAsync("AddMessage", msg);
+			if (msg.room == SimpleMessage.AllGroup)
+			{
+				await Clients.All.SendAsync("AddMessage", msg);
+			}
+			else
+			{
+				await Clients.Group(msg.room).SendAsync("AddMessage", msg);
+			}
+		}
+
+		public async Task JoinGroup(SimpleMessage msg)
+		{
+			await Groups.AddAsync(Context.ConnectionId, msg.room);
+			msg.message = $"＜「{msg.room}」グループに参加しました＞";
+			await Clients.Group(msg.room).SendAsync("AddMessage", msg);
+		}
+
+		public async Task LeaveGroup(SimpleMessage msg)
+		{
+			msg.message = $"＜「{msg.room}」グループから離脱しました＞";
+			await Clients.Group(msg.room).SendAsync("AddMessage", msg);
+			await Groups.RemoveAsync(Context.ConnectionId, msg.room);
 		}
 	}
 }
