90933e35d249140610bc7e7f3cf10c48c4b263f6
lensouko <len@nya-nya.net>
2018-05-06 04:47:25 +0900

    0.3.0に対応

--- SignalBlazorR/SignalBlazorR.Server/Startup.cs
+++ SignalBlazorR/SignalBlazorR.Server/Startup.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
@@ -24,10 +25,13 @@ namespace SignalBlazorR.Server
             });
 			services.AddSignalR();
 
-			services.AddResponseCompression(options =>
+            services.AddResponseCompression(options =>
             {
-                options.MimeTypes = ResponseCompressionDefaults.MimeTypes
-                    .Concat(new[] { MediaTypeNames.Application.Octet });
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+                {
+                    MediaTypeNames.Application.Octet,
+                    WasmMediaTypeNames.Application.Wasm,
+                });
             });
         }
 
